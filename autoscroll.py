812521c39aee9dc66356c3e380eb1edd6e393cf4
Tomek <tiamin@o2.pl>
2021-07-22 23:58:47 +0200

    version 1

--- autoscroll.py
+++ autoscroll.py
@@ -1,7 +1,46 @@
-import mouse
-import time
+from pynput.mouse import Button, Controller, Listener
 
-while not mouse.is_pressed(button='left'):
-    time.sleep(2)
-    print("Waiting for left click...")
-print("End of the program.")
+# TODO
+# class Autoscroll
+# block the mouse pointer and change its look when the middle button is clicked
+# refine delta
+# overwrite middle mouse button behavior from other programs
+
+def on_move(x, y):
+    if scroll_mode:
+        global pos, delta
+        """
+        if y < pos:
+            mouse.scroll(0, 1)
+        elif y > pos:
+            mouse.scroll(0, -1)
+        """
+        d = pos - y
+        if d * delta < 0:
+            delta = d
+        else:
+            delta += d
+        if abs(delta) >= 10:
+            mouse.scroll(0, delta // 3)
+            delta = 0
+        pos = y
+
+def on_click(x, y, button, pressed):
+    if button == Button.right:
+        global end
+        end = True
+    elif button == Button.middle and pressed:
+        global scroll_mode, pos
+        scroll_mode = not scroll_mode
+        pos = mouse.position[1]
+
+mouse = Controller()
+listener = Listener(on_move=on_move, on_click=on_click)
+listener.start()
+end = False
+scroll_mode = False
+delta = 0
+pos = mouse.position[1]
+
+while not end:
+    pass

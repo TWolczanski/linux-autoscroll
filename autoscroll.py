c4634943e6d23d4dc1a11956f7ab6ee53438cad6
Tomek <tiamin@o2.pl>
2021-07-23 16:52:42 +0200

    object approach

--- autoscroll.py
+++ autoscroll.py
@@ -1,46 +1,41 @@
 from pynput.mouse import Button, Controller, Listener
 
 # TODO
-# class Autoscroll
 # block the mouse pointer and change its look when the middle button is clicked
 # refine delta
 # overwrite middle mouse button behavior from other programs
 
-def on_move(x, y):
-    if scroll_mode:
-        global pos, delta
-        """
-        if y < pos:
-            mouse.scroll(0, 1)
-        elif y > pos:
-            mouse.scroll(0, -1)
-        """
-        d = pos - y
-        if d * delta < 0:
-            delta = d
-        else:
-            delta += d
-        if abs(delta) >= 10:
-            mouse.scroll(0, delta // 3)
-            delta = 0
-        pos = y
+class Mouse:
+    def __init__(self):
+        self.controller = Controller()
+        self.pos = self.controller.position[1]
+        self.scroll_mode = False
+    
+    def on_move(self, x, y):
+        if self.scroll_mode:
+            if y < self.pos:
+                self.controller.scroll(0, 1)
+            elif y > self.pos:
+                self.controller.scroll(0, -1)
+            """
+            d = pos - y
+            if d * delta < 0:
+                delta = d
+            else:
+                delta += d
+            if abs(delta) >= 10:
+                mouse.scroll(0, delta // 3)
+                delta = 0
+            """
+            self.pos = y
 
-def on_click(x, y, button, pressed):
-    if button == Button.right:
-        global end
-        end = True
-    elif button == Button.middle and pressed:
-        global scroll_mode, pos
-        scroll_mode = not scroll_mode
-        pos = mouse.position[1]
+    def on_click(self, x, y, button, pressed):
+        if button == Button.middle and pressed:
+            self.scroll_mode = not self.scroll_mode
+            self.pos = self.controller.position[1]
 
-mouse = Controller()
-listener = Listener(on_move=on_move, on_click=on_click)
-listener.start()
-end = False
-scroll_mode = False
-delta = 0
-pos = mouse.position[1]
+# delta = 0
+mouse = Mouse()
 
-while not end:
-    pass
+with Listener(on_move = mouse.on_move, on_click = mouse.on_click) as listener:
+    listener.join()

5c691cb7966d0fc1807c4466c269e0dccbb72b38
Tomek <tiamin@o2.pl>
2021-07-24 17:52:34 +0200

    scrolling improvements

--- autoscroll.py
+++ autoscroll.py
@@ -1,3 +1,4 @@
+import pyautogui
 from pynput.mouse import Button, Controller, Listener
 
 # TODO
@@ -5,37 +6,62 @@ from pynput.mouse import Button, Controller, Listener
 # refine delta
 # overwrite middle mouse button behavior from other programs
 
+SCREEN_HEIGHT = pyautogui.size()[1]
+
 class Mouse:
     def __init__(self):
         self.controller = Controller()
-        self.pos = self.controller.position[1]
+        self.pos = self.controller.position
+        self.scroll_pos = self.controller.position
         self.scroll_mode = False
+        self.delta = 0
+        self.px_to_scr = 10
     
     def on_move(self, x, y):
         if self.scroll_mode:
-            if y < self.pos:
-                self.controller.scroll(0, 1)
-            elif y > self.pos:
-                self.controller.scroll(0, -1)
-            """
-            d = pos - y
-            if d * delta < 0:
-                delta = d
+            d = self.pos[1] - y
+            
+            if self.delta * d < 0:
+                self.delta = d
+                self.scroll_pos = (x, y)
             else:
-                delta += d
-            if abs(delta) >= 10:
-                mouse.scroll(0, delta // 3)
-                delta = 0
+                self.delta += d
+                
+            if self.delta > 0:
+                self.controller.scroll(0, self.delta // self.px_to_scr)
+                self.delta %= self.px_to_scr
+            else:
+                self.controller.scroll(0, -(-self.delta // self.px_to_scr))
+                self.delta = -(-self.delta % self.px_to_scr)
+                
             """
-            self.pos = y
+            while abs(self.delta) >= 10:
+                if self.delta < 0:
+                    self.controller.scroll(0, -1)
+                    self.delta += 10
+                elif self.delta > 0:
+                    self.controller.scroll(0, 1)
+                    self.delta -= 10
+            """
+            
+            self.pos = (x, y)
+            
+            global SCREEN_HEIGHT
+            if self.pos[1] in [0, SCREEN_HEIGHT]:
+                self.controller.position = self.scroll_pos
+                self.pos = self.scroll_pos
 
     def on_click(self, x, y, button, pressed):
-        if button == Button.middle and pressed:
+        if button == Button.right:
+            return False
+        elif button == Button.middle and pressed:
             self.scroll_mode = not self.scroll_mode
-            self.pos = self.controller.position[1]
+            self.pos = (x, y)
+            self.scroll_pos = (x, y)
 
-# delta = 0
 mouse = Mouse()
+# mouse.controller.position = (mouse.controller.position[0], 5000)
+# mouse.pos = mouse.controller.position
 
 with Listener(on_move = mouse.on_move, on_click = mouse.on_click) as listener:
     listener.join()

842e2d4aafaa6610d50458f236f9e456405f97ee
Tomek <tiamin@o2.pl>
2021-07-27 17:12:13 +0200

    deleted previous versions

--- autoscroll.py
+++ autoscroll.py
@@ -1,55 +1,49 @@
 from pynput.mouse import Button, Controller, Listener
+from threading import Event
+import time
 
-# TODO
-# block the mouse pointer and change its look when the middle button is clicked
-# refine delta
-# overwrite middle mouse button behavior from other programs
+def on_move(x, y):
+    global pos, scroll_mode, direction, delay
+    if scroll_mode.is_set():
+        delta = pos[1] - y
+        if delta > 0:
+            direction = 1
+        elif delta < 0:
+            direction = -1
+        else:
+            direction = 0
+        delay = 2 / (abs(delta) + 1)
 
-class Mouse:
-    def __init__(self):
-        self.controller = Controller()
-        self.pos = self.controller.position
-        self.scroll_pos = self.controller.position
-        self.scroll_mode = False
-        self.delta = 0
-        self.px_to_scr = 10
-    
-    def on_move(self, x, y):
-        if self.scroll_mode:
-            self.delta = self.delta + self.pos[1] - y
-            px_to_scr = self.px_to_scr - (0.1 * abs(self.scroll_pos[1] - y)) // 1
-            if px_to_scr < 2:
-                px_to_scr = 2
-                
-            if self.delta > 0:
-                self.controller.scroll(0, self.delta // px_to_scr)
-                self.delta %= px_to_scr
-            elif self.delta < 0:
-                self.controller.scroll(0, -(-self.delta // px_to_scr))
-                self.delta = -(-self.delta % px_to_scr)
-                
-            """
-            while abs(self.delta) >= 10:
-                if self.delta < 0:
-                    self.controller.scroll(0, -1)
-                    self.delta += 10
-                elif self.delta > 0:
-                    self.controller.scroll(0, 1)
-                    self.delta -= 10
-            """
-            
-            self.pos = (x, y)
-            
-    def on_click(self, x, y, button, pressed):
-        if button == Button.right:
-            return False
-        elif button == Button.middle and pressed:
-            self.scroll_mode = not self.scroll_mode
-            self.pos = (x, y)
-            self.scroll_pos = (x, y)
-            self.delta = 0
+def on_click(x, y, button, pressed):
+    global pos, scroll_mode, direction, delay, end
+    if button == Button.right:
+        end = True
+        scroll_mode.set()
+        return False
+    elif button == Button.middle and pressed:
+        if scroll_mode.is_set():
+            scroll_mode.clear()
+        else:
+            pos = (x, y)
+            direction = 0
+            delay = 0
+            scroll_mode.set()
+        
+def autoscroll():
+    global mouse, scroll_mode, direction, delay, end
+    while not end:
+        scroll_mode.wait()
+        while scroll_mode.is_set() and not end:
+            time.sleep(delay)
+            mouse.scroll(0, direction)
 
-mouse = Mouse()
+mouse = Controller()
+scroll_mode = Event()
+listener = Listener(on_move = on_move, on_click = on_click)
+pos = mouse.position
+direction = 0
+delay = 0
+end = False
 
-with Listener(on_move = mouse.on_move, on_click = mouse.on_click) as listener:
-    listener.join()
+listener.start()
+autoscroll()

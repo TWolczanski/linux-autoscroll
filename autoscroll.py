07417b44f25293b5b337064fac6de177c755111c
adeliktas <64136642+adeliktas@users.noreply.github.com>
2021-12-04 16:47:39 +0100

    time.sleep replaced with QTimer

--- autoscroll.py
+++ autoscroll.py
@@ -24,7 +24,8 @@ class QMouseListener(QObject):
         self.listener.start()
 
 class Autoscrollsymbol(QtWidgets.QWidget):
-
+#create a timer with set interval, connect timeout to scroll function and
+#then you can replace scroll_mode = 1 with timer.start() and scroll_mode = 0 with timer.stop()
     def __init__(self, parent=None, windowSize=0, penWidth=0):
         QtWidgets.QWidget.__init__(self, parent)
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.WindowTransparentForInput | QtCore.Qt.Tool)
@@ -41,14 +42,13 @@ class Autoscrollsymbol(QtWidgets.QWidget):
         self.mouse = Controller()
         self.pos = self.mouse.position
         self.direction = 0
-        self.interval = 0
 
         # modify this to adjust the speed of scrolling
-        self.DELAY = 5
+        self.DELAY = 3000
         # modify this to change the button used for using scroll mode
         self.BUTTON_Scroll = Button.middle
         # modify this to change the size (in px) of the area below and above the starting point where the scrolling is paused
-        self.DEAD_AREA = 10
+        self.DEAD_AREA = 20
 
         #Trigger Delay 250ms to avoid instant changes
         self.Triggerdelay = 0.25
@@ -67,10 +67,10 @@ class Autoscrollsymbol(QtWidgets.QWidget):
                 self.direction = 1
             elif delta < 0:
                 self.direction = -1
-            if abs(delta) <= self.DEAD_AREA + self.DELAY * 2:
-                self.interval = 0.5
+            if abs(delta) <= self.DEAD_AREA:
+                self.timer.setInterval(500)
             else:
-                self.interval = self.DELAY / (abs(delta) - self.DEAD_AREA)
+                self.timer.setInterval(int(self.DELAY / (abs(delta) - self.DEAD_AREA)))
         if not self.scroll_mode:
             self.move(x -int(crosssizepx/2),y -int(crosssizepx/2))
 
@@ -88,17 +88,20 @@ class Autoscrollsymbol(QtWidgets.QWidget):
             self.setWindowOpacity(1.0)
             self.pos = (x, y)
             self.direction = 0
-            self.interval = 0
+            self.timer.setInterval(100)
             self.scroll_mode = 1
+            self.timer.start()
             self.Timestart = time.time()
         elif button == self.BUTTON_Scroll and not pressed and self.scroll_mode:
             self.Timeend = time.time()
             self.Timedelta = self.Timeend - self.Timestart
             if self.Timedelta>self.Triggerdelay:
                 self.scroll_mode = 0
+                self.timer.stop()
                 self.setWindowOpacity(0.0)
         elif (button == self.BUTTON_Scroll or button==Button.left) and pressed and self.scroll_mode:
             self.scroll_mode = 0
+            self.timer.stop()
             self.setWindowOpacity(0.0)
         
         if (button == Button.middle) and pressed:
@@ -113,16 +116,15 @@ class Autoscrollsymbol(QtWidgets.QWidget):
         painter.drawLine(0, int(crosssizepx/2), crosssizepx, int(crosssizepx/2))#-
         painter.drawLine(int(crosssizepx/2), 0, int(crosssizepx/2), crosssizepx)#|
         self.setWindowOpacity(0.0)
-
+        
     def scrolldown(self):
-        if self.scroll_mode:
-            time.sleep(self.interval)
-            self.mouse.scroll(0, self.direction)
+        self.mouse.scroll(0, self.direction)
 
     def autoscroll(self):
         self.timer = QTimer(self)
+        self.timer.setInterval(100)
         self.timer.timeout.connect(self.scrolldown)
-        self.timer.start(1)
+        self.timer.start()
 
     def mouselistener(self):
         self.listener=Listener(on_move=self.on_move)
